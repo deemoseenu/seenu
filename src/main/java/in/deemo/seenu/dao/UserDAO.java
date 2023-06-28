6b3ec40b9e8da1e28f0147fc8b89e8eefd50d114
deemoseenu <siva.gopi@fwsa.freshworks.com>
2023-06-28 15:06:02 +0530

    Add the update,delate

--- src/main/java/in/deemo/seenu/dao/UserDAO.java
+++ src/main/java/in/deemo/seenu/dao/UserDAO.java
@@ -10,10 +10,63 @@ public class UserDAO {
 	
 	public void create(User newUser) {
 		
-		Userlist.listofUsers[0] = newUser;
+		//Userlist.listofUsers[0] = newUser;
+		
+		User[] array =Userlist.listofUsers;
+		
+		for (int i=0;i<array.length;i++) {
+			
+			User user = array[i];
+			
+			if(user == null) {
+				array[i]=newUser;
+			//	user=newUser;
+				break;
+			}
+			
+		}
 		
 		
 	}
+	public void update(int id, User updatedUser) {
+		User[] userList = Userlist.listofUsers;
+
+		for (int i = 0; i < userList.length; i++) {
+			User user = userList[i];
+			if (user.getId() == id) {
+				userList[i].setFirstName(updatedUser.getFirstName());
+				userList[i].setLastName(updatedUser.getLastName());
+				break;
+			}
+		}
+	}
+	
+	public void delete(int userId) {
+		User[] userList = Userlist.listofUsers;
+		for (int i = 0; i < userList.length; i++) {
+			User user = userList[i];
+			if (user.getId() == userId) {
+				user.setActive(false);
+				break;
+			}
+		}
+	}
+	
+	public User findById(int userId) {
+		User[] userList = Userlist.listofUsers;
+		User userMatch = null;
+
+		for (int i = 0; i < userList.length; i++) {
+			User user = userList[i];
+			if (user.getId() == userId) {
+				userMatch = user;
+				break;
+			}
+		}
+		return userMatch;
+	}
+	
+	
 	
 		
 
--- src/main/java/in/deemo/seenu/service/UserService.java
+++ src/main/java/in/deemo/seenu/service/UserService.java
@@ -25,9 +25,40 @@ public class UserService {
 		newUser.setEmail("Sivagopi777@gmail.com");
 		newUser.setPassword("S1i2v3@A4");
 		newUser.setActive(true);
-
+		
+		
+		User newUser1 = new User();
+		newUser.setId(001);
+		newUser.setFirstName("mahi");
+		newUser.setLastName("seenu");
+		newUser.setEmail("mahiseenu777@gmail.com");
+		newUser.setPassword("S1i2v3@A4");
+		newUser.setActive(true);
+		
+		User newUser2 = new User();
+		newUser.setId(002);
+		newUser.setFirstName("mahi");
+		newUser.setLastName("seenu");
+		newUser.setEmail("mahiseenu777@gmail.com");
+		newUser.setPassword("S1i2v3@A4");
+		newUser.setActive(true);
+		
 		UserDAO userDao = new UserDAO();
 		userDao.create(newUser);
+		userDao.create(newUser1);
+		userDao.create(newUser2);
+		
+	
+		
 	}
+	
 
+
+public void update() {
+	User updateUser = new User();
+	
+	UserDAO userDao = new UserDAO();
+	userDao.update(002, updateUser);
+	
+}
 }
